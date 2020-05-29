f02e07f5779da6b5e95c9685556d7923829b9282
Juan José Pérez <juanjoseperez@MacBook-Pro-de-Juan.local>
2020-05-29 20:09:31 +0200

    clone merge

--- exercises.js
+++ exercises.js
@@ -5,4 +5,12 @@ const Init = (arr) => arr.slice(0,arr.length-1);
 const Last = (arr) => arr.slice(-1);
 
 // 2. Concat
-const concat = (a, b) => {};
+const concat = (...nArr) => [].concat(...nArr);
+
+// 3. Clone Merge
+const shallowClone = (source) => { return { ...source }};
+const deepClone = (source) => JSON.parse(JSON.stringify(source));
+
+const merge = (source, target) => { return  { ...target, ...source }};
+
+// 4. Read Books
