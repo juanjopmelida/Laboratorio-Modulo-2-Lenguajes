b570e4dfbb02b86ff3ef93344c7da8f6badcb1cc
Juan José Pérez <juanjoseperez@MacBook-Pro-de-Juan.local>
2020-06-12 19:23:46 +0200

    Almost done

--- exercises.js
+++ exercises.js
@@ -1,38 +1,45 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
+const firstArr = [1, 3, 5, 7, 11];
+const secondArr = [2, 4, 6];
 // 1. Array operations
-var Head = function (_a) {
-    var first = _a[0], rest = _a.slice(1);
-    return first;
-};
-var Tail = function (_a) {
-    var first = _a[0], rest = _a.slice(1);
-    return rest;
-};
-var Init = function (arr) { return arr.slice(0, arr.length - 1); };
-var Last = function (arr) { return arr.slice(-1); };
+const head = ([first, ...rest]) => first;
+console.log(`Head function: ${head(firstArr)}`);
+const tail = ([first, ...rest]) => rest;
+console.log(`Tail function: ${tail(firstArr)}`);
+const init = (arr) => arr.slice(0, arr.length - 1);
+console.log(`Init function: ${init(firstArr)}`);
+const last = (arr) => arr.slice(-1);
+console.log(`Last function: ${last(firstArr)}`);
 // 2. Concat
-var concat = function () {
-    var nArr = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        nArr[_i] = arguments[_i];
-    }
-    return [].concat.apply([], nArr);
-};
+const concat = (...nArr) => [].concat(...nArr);
+console.log(`concat function: ${concat(firstArr)}`);
 // 3. Clone Merge
-var shallowClone = function (source) { return __assign({}, source); };
-var deepClone = function (source) { return JSON.parse(JSON.stringify(source)); };
-var merge = function (source, target) { return __assign(__assign({}, target), source); };
-var isBookRead = function (books, titleToSearch) {
-    return true;
-};
-var a = 0;
+const shallowClone = (source) => (Object.assign({}, source));
+console.log(`shallowClone function: ${shallowClone(firstArr)}`);
+const deepClone = (source) => JSON.parse(JSON.stringify(source));
+console.log(`deepClone function: ${deepClone(firstArr)}`);
+const merge = (source, target) => (Object.assign(Object.assign({}, target), source));
+console.log(`merge function: ${merge(firstArr, secondArr)}`);
+const books = [
+    { title: 'Harry Potter y la piedra filosofal', isRead: true },
+    { title: 'Canción de hielo y fuego', isRead: false },
+    { title: 'Devastación', isRead: true },
+];
+const isBookRead = (title) => books.find(b => b.title === title).isRead;
+// 5. Slot Machine
+class SlothMachine {
+    constructor() {
+        this.play = () => {
+            this.award += 1;
+            this.val1 = Math.random() > 0.5 ? true : false;
+            this.val2 = Math.random() > 0.5 ? true : false;
+            this.val3 = Math.random() > 0.5 ? true : false;
+            console.log(this.val1, this.val2, this.val3);
+            if ((this.val1 === this.val2) && (this.val2 === this.val3)) {
+                console.log(`Congratulations!!!. You won ${this.award} coins!!`);
+            }
+            else {
+                console.log(`Good luck next time!!`);
+            }
+        };
+    }
+}
--- exercises.ts
+++ exercises.ts
@@ -1,24 +1,62 @@
+const firstArr: number[] = [1, 3, 5, 7, 11];
+const secondArr: number[] = [2, 4, 6];
+
 // 1. Array operations
-const Head = ([first, ...rest]) => first;
-const Tail = ([first, ...rest]) => rest;
-const Init = (arr) => arr.slice(0,arr.length-1);
-const Last = (arr) => arr.slice(-1);
+const head = ([first, ...rest]: any) => first;
+console.log(`Head function: ${head(firstArr)}`);
+const tail = ([first, ...rest]: any) => rest;
+console.log(`Tail function: ${tail(firstArr)}`);
+const init = (arr: any) => arr.slice(0, arr.length - 1);
+console.log(`Init function: ${init(firstArr)}`);
+const last = (arr: any) => arr.slice(-1);
+console.log(`Last function: ${last(firstArr)}`);
 
 // 2. Concat
 const concat = (...nArr) => [].concat(...nArr);
+console.log(`concat function: ${concat(firstArr)}`);
 
 // 3. Clone Merge
-const shallowClone = (source) => { return { ...source }};
+const shallowClone = (source) => ({ ...source });
+console.log(`shallowClone function: ${shallowClone(firstArr)}`);
 const deepClone = (source) => JSON.parse(JSON.stringify(source));
+console.log(`deepClone function: ${deepClone(firstArr)}`);
 
-const merge = (source, target) => { return  { ...target, ...source }};
+const merge = (source : number[], target : number[]) => ({ ...target, ...source });
+console.log(`merge function: ${merge(firstArr, secondArr)}`);
 
-interface book {
+// 4. Read Books
+interface Book {
     title: string,
     isRead: boolean
 }
 
-const isBookRead = (books: book[], titleToSearch: string): boolean  => {
-    return true;
+const books : Book[] = [
+  { title: 'Harry Potter y la piedra filosofal', isRead: true },
+  { title: 'Canción de hielo y fuego', isRead: false },
+  { title: 'Devastación', isRead: true },
+];
+
+const isBookRead = (title) => books.find(b =>  b.title === title).isRead;
+
+// 5. Slot Machine
+
+class SlothMachine {
+  award: number;
+  val1: boolean;
+  val2: boolean;
+  val3: boolean;
+
+  play = () => {
+    this.award += 1;
+    this.val1 = Math.random() > 0.5 ? true : false;
+    this.val2 = Math.random() > 0.5 ? true : false;
+    this.val3 = Math.random() > 0.5 ? true : false;
+    console.log(this.val1, this.val2, this.val3);
+    if ((this.val1 === this.val2) && (this.val2 === this.val3)) {
+      console.log(`Congratulations!!!. You won ${this.award} coins!!`);
+    } 
+    else{
+      console.log(`Good luck next time!!`);
+    }
+  }
 }
-const a = 0;
